33cfa3360d05cc2a86065a0472ddf74b7c641b33
HockLeongWee <20046765@myrp.edu.sg>
2021-06-22 02:02:25 +0800

    2nd Interface done (no list view yet)

--- app/src/main/java/sg/edu/rp/c346/id20046765/holidayhomework/ItemListActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20046765/holidayhomework/ItemListActivity.java
@@ -3,12 +3,38 @@ package sg.edu.rp.c346.id20046765.holidayhomework;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class ItemListActivity extends AppCompatActivity {
+
+    EditText etName;
+    EditText etYear;
+    EditText etMonth;
+    EditText etDay;
+    Spinner spnMonth;
+    Button btnAdd;
+    Button btnUpdate;
+    Button btnDelete;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_list);
+
+        etName = findViewById(R.id.etName);
+        etYear = findViewById(R.id.etYear);
+        etMonth = findViewById(R.id.etMonth);
+        etDay = findViewById(R.id.etDay);
+        spnMonth = findViewById(R.id.spnMonth);
+        btnAdd = findViewById(R.id.btnAdd);
+        btnUpdate = findViewById(R.id.btnUpdate);
+        btnDelete = findViewById(R.id.btnDelete);
+
+        ArrayAdapter adapterForSpinner = ArrayAdapter.createFromResource(this, R.array.spinnerMonths, R.layout.spinnertext);
+        spnMonth.setAdapter(adapterForSpinner);
+
     }
 }
